8ce79847494019cdd8ba0253c2f77e3eed5d88b5
wlisses <wlissesfernando285@gmail.com>
2023-04-13 19:03:26 -0300

    alterado cors

--- api/app.js
+++ api/app.js
@@ -12,7 +12,7 @@ app.use('/images', express.static(`${__dirname}/images`));
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: '*',
 }));
 
 app.use('/user', userRouter);
